03dbf4d863032b0e824e6685a8635da98891e801
IsraelPeled <ipb14ck0ut@gmail.com>
2024-11-04 15:30:21 +0200

    Connecting MongoDB Database

---
+++ configuration.py
@@ -0,0 +1,14 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
+uri = os.getenv("DATABASE_URL")
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+db = client.characters_db
+collection = db["characters_data"]
