63f79cc0d5aed425660211b1caf7faac28f1477d
IsraelPeled <ipb14ck0ut@gmail.com>
2024-11-11 17:33:06 +0200

    Fixing __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 from src.characters.routers import router
 from fastapi.middleware.cors import CORSMiddleware
+from middleware import register_middleware
 
 
 version="v1"
-
-allow_origins=["http://127.0.0.1:8000", "http://0.0.0.0:8000"]
 
 app = FastAPI(
     title= "DisneyAPI",
@@ -22,13 +21,6 @@ app = FastAPI(
     
 )
 
-#TODO: ADDING CORSMiddleware
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=allow_origins,  # Allows all origins, can be replaced with specific URLs
-    allow_credentials=True,
-    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allows all headers
-)
+register_middleware(app)
 
 app.include_router(router=router, prefix=f"/api/{version}/characters", tags=["characters"])
