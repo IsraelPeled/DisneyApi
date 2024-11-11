11cc9c7ae3ae7b3ef9a7abbb3aa39c660a285f95
IsraelPeled <ipb14ck0ut@gmail.com>
2024-11-11 18:14:10 +0200

    Fixing configuration

--- configuration.py
+++ configuration.py
@@ -8,7 +8,12 @@ load_dotenv()
 uri = os.getenv("DATABASE_URL")
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(
+    uri,
+    tls=True,  # Explicitly enable TLS/SSL
+    tlsAllowInvalidCertificates=False,  # Ensure the certificate is valid
+    server_api=ServerApi('1')
+    )
 
 db = client.characters_db
 collection = db["characters_data"]
